9d79c6606423c31d8b3fb7a5d18372b83397e876
BrydenIsNotSmart <brydenwcgames@gmail.com>
2022-12-23 20:02:00 -0600

    add mark

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   port: 80,
   mongo: process.env.mongo,
-  ownerids: ["529815278456930314", "700609775838298113"],
+  ownerids: ["529815278456930314", "700609775838298113", "272442568275525634"],
   staff: ["529815278456930314", "700609775838298113"],
   vlAPIKEY: "RkVoB2CjeyOjCputjxfz",
   bot: {
