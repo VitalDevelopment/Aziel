bf626be93a85680ffc0e1b180295797c77b72587
BrydenIsNotSmart <brydenwcgames@gmail.com>
2023-01-05 16:38:42 -0600

    changes

--- src/server/routers/giveaway.js
+++ src/server/routers/giveaway.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
    let giveaway = await global.giveawayModel.findOne({ messageid: req.params.id });
+   if(!giveaway) res.redirect("/404?message=Giveaway Id does not exist.");
     let guild = await client.guilds.fetch(giveaway.guildid)
+    if(!guild) res.redirect("/404?message=This server has no giveaways");
      guild.iconURL = `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`;
      giveaway.host = await client.users.fetch(giveaway.hostedBy);
 
